13ade4a2d962087fc07d0ba1c4dc4d1bb8f6843c
ReTims <renatatims@gmail.com>
2022-08-12 13:02:17 -0500

    Confirm and Prompt windows added to the function

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword () {
+confirm ("Your password must contain at least 8 characters (no more than 128) and also include at least 1 element of the following types:\nLowercase letter,\nUppercase letter, \nNumber and/or\nSpecial character. ")
+prompt("Please enter the number of characters (OBS. It requires a minimum of 8 characters and maximun of 128");
+confirm("Would you like to include lowercase letters in your new password?");
+confirm("Would you like to include UPPERCASE leters in your password?");
+confirm ("Would you like to include numbers in your new password?");
+confirm ("Would you like to include special characters in your new password?")
+  
+}
+
+
